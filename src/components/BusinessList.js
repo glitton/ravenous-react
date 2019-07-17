969115a2690c0e56ec2baee6e3947cf9c8d68822
Generosa Litton <glitton@newrelic.com>
2019-07-16 23:32:25 -0700

    added bizlist and biz components

---
+++ src/components/BusinessList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./BusinessList.css";
+import Business from "./Business";
+
+const BusinessList = () => {
+  return (
+    <div className="BusinessList">
+      <Business />
+    </div>
+  );
+};
+
+export default BusinessList;
