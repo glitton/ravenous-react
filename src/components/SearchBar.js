fc102d49a472de4aaa031f840634cc3004bb584e
Generosa Litton <glitton@newrelic.com>
2019-07-19 16:27:09 -0700

    working on changing colors in sort by

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,50 @@
 import React from "react";
 import "./SearchBar.css";
 
-const sortByOptions = {
-  "Best Match": "best_match",
-  "Highest Rated": "rating",
-  "Most Reviewed": "review_count"
-};
-
 class SearchBar extends React.Component {
-  renderSortByOption = () => {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+  sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count"
+  };
+
+  state = {
+    term: "",
+    location: "",
+    sortBy: "best_match"
+  };
+
+  getSortByClass = sortByOption => {
+    console.log(sortByOption);
+    return this.state.sortBy === sortByOption ? "active" : "";
+  };
+
+  handleSortByChange = evt => {
+    evt.preventDefault();
+    console.log("Clicked");
+    this.setState({ sortBy: evt.target.value });
+  };
+
+  renderSortByOptions = () => {
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
+      return (
+        <li
+          key={sortByOptionValue}
+          className={this.getSortByClass(sortByOptionValue)}
+          onClick={this.handleSortByChange}
+        >
+          {sortByOption}
+        </li>
+      );
     });
   };
+
   render() {
     return (
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
-          <ul>{this.renderSortByOption()}</ul>
+          <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
           <input placeholder="Search Businesses" />
